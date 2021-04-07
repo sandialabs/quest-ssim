06116e6b55df29f8ac6cb0e2181e53e2afc7b8a7
utamrak <utamrak@sandia.gov>
2021-04-07 17:33:54 -0600

    Added tests `test_DSSModel_positive_sequence_voltage()` and `test_DSSModel_complex_voltage()`.

--- ssim/tests/test_opendss.py
+++ ssim/tests/test_opendss.py
@@ -53,6 +53,18 @@ def test_DSSModel_node_voltage(test_circuit):
     test_circuit.solve(10*3600)
     assert voltage != pytest.approx(test_circuit.node_voltage('loadbus1'))
 
+def test_DSSModel_positive_sequence_voltage(test_circuit):
+    test_circuit.solve(0)
+    voltage = test_circuit.positive_sequence_voltage('loadbus1')
+    test_circuit.solve(10*3600)
+    assert voltage != pytest.approx(test_circuit.
+                                    positive_sequence_voltage('loadbus1'))
+
+def test_DSSModel_complex_voltage(test_circuit):
+    test_circuit.solve(0)
+    voltage = test_circuit.complex_voltage('loadbus1')
+    test_circuit.solve(10 * 3600)
+    assert voltage != pytest.approx(test_circuit.complex_voltage('loadbus1'))
 
 def test_DSSModel_storage(test_circuit):
     test_circuit.add_storage(
