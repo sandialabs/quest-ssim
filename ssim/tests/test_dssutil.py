d88e529297b95f0687eefbbe08d10d55566f48bc
William Flynn Vining <wfvinin@sandia.gov>
2021-05-04 14:20:18 -0600

    Add API to ssim.opendss.DSSModel for line/switch failures
    In OpenDSS switches are just special lines, so this API works
    for both elements. A line can be failed open, closed, or current.
    
    When a line is failed any switch controllers associated with the
    are locked to prevent normal operation. If/when an API is added
    for "normal" switch operations extra care will need to be taken
    that the switch controls have been restored to a functional state.
    For now, since no such API exists, it suffices to ensure that the
    switch controls are unlocked after the line is restored.

--- ssim/tests/test_dssutil.py
+++ ssim/tests/test_dssutil.py
@@ -47,3 +47,49 @@ def test_to_dataframe(simple_circuit):
     assert (df["Phases"] == 3).all()
     assert "line1" in df.index
     assert "line2" in df.index
+
+
+def test_open(simple_circuit):
+    assert not dssdirect.CktElement.IsOpen(1, 1)
+    dssutil.open_terminal("line.line1", 2)
+    dssdirect.Circuit.SetActiveElement("line.line1")
+    assert dssdirect.CktElement.IsOpen(2, 1)
+    assert dssdirect.CktElement.IsOpen(2, 2)
+    assert dssdirect.CktElement.IsOpen(2, 3)
+
+
+def test_close(simple_circuit):
+    dssutil.open_terminal("line.line2", 1, 1)
+    dssdirect.Circuit.SetActiveElement("line.line2")
+    assert dssdirect.CktElement.IsOpen(1, 1)
+    dssutil.close_terminal("line.line2", 1, 1)
+    dssdirect.Circuit.SetActiveElement("line.line2")
+    assert not dssdirect.CktElement.IsOpen(1, 1)
+
+
+def test_lock_unlock_switch(simple_circuit):
+    dssutil.run_command("new swtcontrol.swc1"
+                        " delay=0.0"
+                        " normal=closed"
+                        " SwitchedObj=line.line2"
+                        " SwitchedTerm=1"
+                        " enabled=y")
+    dssutil.run_command("set mode=time stepsize=10s controlmode=time")
+    dssdirect.Solution.SolvePlusControl()
+    dssdirect.SwtControls.Name("swc1")
+    dssdirect.SwtControls.Action(1)  # open the switch
+    dssdirect.Solution.SolvePlusControl()
+    dssdirect.Circuit.SetActiveElement("line.line2")
+    assert dssdirect.CktElement.IsOpen(2, 1)
+    dssutil.lock_switch_control("line.line2")
+    dssdirect.SwtControls.Name("swc1")
+    assert dssdirect.SwtControls.IsLocked()
+    dssdirect.SwtControls.Action(2)
+    dssdirect.Solution.SolvePlusControl()
+    dssdirect.Circuit.SetActiveElement("line.line2")
+    assert dssdirect.CktElement.IsOpen(2, 1)
+    dssutil.unlock_switch_control("line.line2")
+    dssdirect.Solution.SolvePlusControl()
+    dssdirect.SwtControls.Name("swc1")
+    state = dssdirect.SwtControls.State()
+    assert not dssdirect.SwtControls.IsLocked()
