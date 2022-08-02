93710e1e814732e5b478ce1a95cb35a514aaa6ae
Will Vining <wfvinin@sandia.gov>
2022-08-02 12:35:09 -0600

    Better layout, dummy method for button press

--- ssim/ui/kivy/ssimapp.py
+++ ssim/ui/kivy/ssimapp.py
@@ -1,22 +1,27 @@
 """Storage Sizing and Placement Kivy application"""
-
+from kivy.logger import Logger, LOG_LEVELS
 from kivy.app import App
-from kivy.uix.widget import Widget
+from kivy.uix.screenmanager import Screen
 
 from ssim.ui import Project
+
 
 class SSimApp(App):
 
     def __init__(self, *args, **kwargs):
         self.project = Project("unnamed") # TODO name
-        super().__init__(*args, **kwargs)
+        super(SSimApp, self).__init__(*args, **kwargs)
 
     def build(self):
         return SSim()
 
 
-class SSim(Widget):
-    pass
+class SSim(Screen):
+
+    def report(self, message):
+        Logger.debug("button pressed: %s", message)
+
 
 if __name__ == '__main__':
+    Logger.setLevel(LOG_LEVELS["debug"])
     SSimApp().run()
