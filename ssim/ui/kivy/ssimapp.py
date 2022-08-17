0df6962ce602d0a0243724088ee4dec17cff4a5f
John Eddy <jpeddy@sandia.gov>
2022-08-17 10:29:29 -0600

    Added popup messaging lack of grid model when trying to create/edit voltage metrics.

--- ssim/ui/kivy/ssimapp.py
+++ ssim/ui/kivy/ssimapp.py
@@ -1,13 +1,19 @@
 """Storage Sizing and Placement Kivy application"""
 from kivy.logger import Logger, LOG_LEVELS
 from kivy.app import App
+from kivymd.app import MDApp
+from kivymd.uix.list import OneLineListItem, TwoLineListItem
+from kivy.uix.popup import Popup
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.properties import ObjectProperty
 
 from ssim.ui import Project
 
 
-class SSimApp(App):
+class SSimApp(MDApp):
 
     def __init__(self, *args, **kwargs):
         self.project = Project("unnamed") # TODO name
@@ -54,9 +60,54 @@ class DERConfigurationScreen(SSimBaseScreen):
 class LoadConfigurationScreen(SSimBaseScreen):
     pass
 
+class NoGridPopupContent(BoxLayout):
+    pass
 
 class MetricConfigurationScreen(SSimBaseScreen):
-    pass
+
+    def configure_voltage_metrics(self):
+        self.load_bussed_into_list()
+        print("I'm passing on the voltage issue...")
+
+    def configure_some_other_metrics(self):
+        self.ids.interlist.clear_widgets()
+        self.ids.metriclist.clear_widgets()
+        self.ids.interlabel.text = "Metric Objects"
+        print("I'm passing on the other issue...")
+
+    def load_bussed_into_list(self):
+        #busses = self.project.bus_names()
+        self.ids.interlist.clear_widgets()
+        self.ids.metriclist.clear_widgets()
+        self.ids.interlabel.text = "Busses"
+
+        if self.project._grid_model is None:
+            content = NoGridPopupContent()
+            content.orientation="vertical"
+            #box = BoxLayout(orientation="vertical")
+            #box.add_widget(Label(
+            #    text='',
+            #    text_size=self.size
+            #    ))
+            #disbut = Button(text='Dismiss')
+            #box.add_widget(disbut)
+
+            popup = Popup(
+                title='No Grid Model', content=content, auto_dismiss=False,
+                size_hint=(0.4, 0.4)
+                )
+            content.ids.dismissBtn.bind(on_press=popup.dismiss)
+            #disbut.bind(on_press=popup.dismiss)
+
+            # open the popup
+            popup.open()
+            return
+
+        list = self.ids.interlist
+        busses = self.project._grid_model.bus_names
+        for x in busses:
+            item = OneLineListItem(text=str(x))
+            list.add_widget(item)
 
 
 class LoadConfigurationScreen(SSimBaseScreen):
