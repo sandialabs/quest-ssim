9cf1c16c82633e89f9f9db5e7a63d225eafa1437
Will Vining <wfvinin@sandia.gov>
2023-08-23 09:23:00 -0600

    Record raw voltage data at all buses with voltage metrics recorded

--- ssim/ui/kivy/ssimapp.py
+++ ssim/ui/kivy/ssimapp.py
@@ -2845,12 +2845,22 @@ class ResultsDetailScreen(SSimBaseScreen):
             _, data_storage_voltage = result.storage_voltages()
             data_storage_voltage.drop(['time'], axis=1, inplace=True)
 
+            _, all_bus_voltage = result.bus_voltages()
+            all_bus_voltage.drop(["time"], axis=1, inplace=True)
+            # rename the columns
+            all_bus_voltage.columns = [
+                col + '_bus_voltage' for col in all_bus_voltage.columns
+            ]
+
             # rename the 'data_storage_voltage' by appending '_voltage' to each header
             new_col_names = [item + '_voltage' for item in data_storage_voltage.columns]
             data_storage_voltage.columns = new_col_names
 
             # combine all data into a single dataframe
-            data = pd.concat([data_storage_state, data_storage_voltage],axis=1)
+            data = pd.concat(
+                [data_storage_state, data_storage_voltage, all_bus_voltage],
+                axis=1
+            )
             config_dir = os.path.basename(os.path.normpath(result.config_dir))
             config_key = self.config_id_to_name[config_dir]
             
@@ -3024,6 +3034,7 @@ class ResultsDetailScreen(SSimBaseScreen):
             # extract the data
             storage_state_headers, storage_state_data = current_result.storage_state()
             storage_voltage_headers, storage_voltage_data = current_result.storage_voltages()
+            bus_voltage_headers, bus_voltage_data = current_result.bus_voltages()
             
             # remove 'time' from the header list and pandas data frame to prevent
             # duplication
@@ -3031,12 +3042,24 @@ class ResultsDetailScreen(SSimBaseScreen):
                 storage_voltage_headers.pop(0)
             storage_voltage_data.drop(['time'], axis=1, inplace=True)
 
+            if bus_voltage_headers is not None:
+                bus_voltage_headers.pop(0)
+            bus_voltage_data.drop(['time'], axis=1, inplace=True)
+
             # 'storage_voltage_headers' have no indication that these labels
             # represent voltage, append string '_voltage' to each label
-            storage_voltage_headers = [item + '_voltage' for item in storage_voltage_headers]                                                                                                                            
-            
-            self.list_items = storage_state_headers + storage_voltage_headers
-            self.variable_data = pd.concat([storage_state_data, storage_voltage_data], axis=1)
+            storage_voltage_headers = [item + '_voltage' for item in storage_voltage_headers]
+            bus_voltage_headers = [item + '_bus_voltage' for item in bus_voltage_headers]
+
+            self.list_items = (
+                storage_state_headers
+                + storage_voltage_headers
+                + bus_voltage_headers
+            )
+            self.variable_data = pd.concat(
+                [storage_state_data, storage_voltage_data, bus_voltage_data],
+                axis=1
+            )
 
             self.x_data = list(self.variable_data.loc[:, 'time'])
 
